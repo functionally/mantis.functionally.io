76af1a72f77c2ea124cb39591cab5eb3e27f39f0
Brian W Bush <code@functionally.io>
2021-04-05 08:03:08 -0600

    updated documentation for SOFR and added JSON schemas

--- scripts/oracle.sh
+++ scripts/oracle.sh
@@ -14,7 +14,7 @@ echo Timestamp: $TIMESTAMP
 
 curl -s -o "$HTML" https://www.eia.gov/todayinenergy/prices.php
 
-CID=$(ipfs add -q "$HTML" | sed -e 's/^added \(.*\) .*$/\1/')
+CID=$(ipfs add --quiet --pin=false "$HTML" | sed -e 's/^added \(.*\) .*$/\1/')
 
 echo CID: $CID
 
@@ -31,7 +31,7 @@ curl -H "x-rapidapi-key: $(cat rapidapi.secret)" \
      -H "x-rapidapi-host: apidojo-yahoo-finance-v1.p.rapidapi.com" \
      -H "useQueryString: true" \
      -s 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=SR1J21.CME,SR1K21.CME,SR1M21.CME,SR1N21.CME,SR1Q21.CME,SR1U21.CME,SR1V21.CME,SR1X21.CME,SR1Z21.CME,SR1F22.CME,SR1G22.CME,SR1H22.CME,SR3H21.CME,SR3M21.CME,SR3U21.CME,SR3Z21.CME,SR3H22.CME,SR3M22.CME,SR3U22.CME,SR3Z22.CME,SR3H23.CME,SR3M23.CME,SR3U23.CME,SR3Z23.CME' \
-| jq '{ Timestamp : "'$TIMESTAMP'" , Schema : "https://mantis.functionally.io/schema/247427/v1.json" , "SOFR_1MONTH" : [ .quoteResponse.result | .[] | select(.underlyingSymbol == "SR1.CME") | { (.symbol) : { settle : .expireIsoDate[0:10], open : (.regularMarketOpen | tostring) } } ] | add  , "SOFR_3MONTH" : [ .quoteResponse.result | .[] | select(.underlyingSymbol == "SR3.CME") | { (.symbol) : { settle : .expireIsoDate[0:10], open : (.regularMarketOpen | tostring) } } ] | add  }' \
+| jq '{ Timestamp : "'$TIMESTAMP'" , "Processor" : "https://mantis.functionally.io/mantis.metadata.json" , Schema : "https://mantis.functionally.io/schema/247427/v1.json" , "SOFR_1MONTH" : [ .quoteResponse.result | .[] | select(.underlyingSymbol == "SR1.CME") | { (.symbol) : { settle : .expireIsoDate[0:10], open : (.regularMarketOpen | tostring) } } ] | add  , "SOFR_3MONTH" : [ .quoteResponse.result | .[] | select(.underlyingSymbol == "SR3.CME") | { (.symbol) : { settle : .expireIsoDate[0:10], open : (.regularMarketOpen | tostring) } } ] | add  }' \
 | sed -e 's/SR.F/JAN20/;s/SR.G/FEB20/;s/SR.H/MAR20/;s/SR.J/APR20/;s/SR.K/MAY20/;s/SR.M/JUN20/;s/SR.N/JUL20/;s/SR.Q/AUG20/;s/SR.U/SEP20/;s/SR.V/OCT20/;s/SR.X/NOV20/;s/SR.Z/DEC20/;s/\.CME//' \
 > sofr-futures.json
 
@@ -43,7 +43,7 @@ jq -s add energy.json sofr.json > $JSON
 less $JSON
 
 
-DIR_CID=$(ipfs add -q -r ../data | tail -n 1)
+DIR_CID=$(ipfs add --quiet --pin=false --recursive ../data | tail -n 1)
 
 ipfs name publish --key=mantis /ipfs/$DIR_CID
 
